1e789f6bd1d9068b8f64c1f84e5a9f9a362b6232
Joshua Avalon <git@avalon.sh>
2026-02-18 22:37:23 +0800

    feat: Add attachments

---
+++ pb_migrations/1771425425_updated_games.js
@@ -0,0 +1,28 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_879072730")
+
+  // add field
+  collection.fields.addAt(17, new Field({
+    "cascadeDelete": false,
+    "collectionId": "pbc_579055366",
+    "hidden": false,
+    "id": "relation1204091606",
+    "maxSelect": 999,
+    "minSelect": 0,
+    "name": "attachments",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "relation"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_879072730")
+
+  // remove field
+  collection.fields.removeById("relation1204091606")
+
+  return app.save(collection)
+})
