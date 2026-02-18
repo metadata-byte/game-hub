876b27f7c22989d1ed2892236a20c00386afa856
Joshua Avalon <git@avalon.sh>
2026-02-19 00:25:15 +0800

    chore: Fix lint

--- pb_migrations/1771425425_updated_games.js
+++ pb_migrations/1771425425_updated_games.js
@@ -1,28 +1,28 @@
 /// <reference path="../pb_data/types.d.ts" />
-migrate((app) => {
-  const collection = app.findCollectionByNameOrId("pbc_879072730")
+migrate(app => {
+  const collection = app.findCollectionByNameOrId("pbc_879072730");
 
   // add field
   collection.fields.addAt(17, new Field({
-    "cascadeDelete": false,
-    "collectionId": "pbc_579055366",
-    "hidden": false,
-    "id": "relation1204091606",
-    "maxSelect": 999,
-    "minSelect": 0,
-    "name": "attachments",
-    "presentable": false,
-    "required": false,
-    "system": false,
-    "type": "relation"
-  }))
+    cascadeDelete: false,
+    collectionId: "pbc_579055366",
+    hidden: false,
+    id: "relation1204091606",
+    maxSelect: 999,
+    minSelect: 0,
+    name: "attachments",
+    presentable: false,
+    required: false,
+    system: false,
+    type: "relation"
+  }));
 
-  return app.save(collection)
-}, (app) => {
-  const collection = app.findCollectionByNameOrId("pbc_879072730")
+  return app.save(collection);
+}, app => {
+  const collection = app.findCollectionByNameOrId("pbc_879072730");
 
   // remove field
-  collection.fields.removeById("relation1204091606")
+  collection.fields.removeById("relation1204091606");
 
-  return app.save(collection)
-})
+  return app.save(collection);
+});
